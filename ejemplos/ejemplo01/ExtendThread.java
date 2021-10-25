fcffbcaf1ef86f34fcfe888e563be2d2c0914ba0
ruben-ies1javea <67370618+ruben-ies1javea@users.noreply.github.com>
2021-10-25 10:21:15 +0200

    Add files via upload

---
+++ ejemplos/ejemplo01/ExtendThread.java
@@ -0,0 +1,18 @@
+class ExtendThread{
+     public static void main(String[] args) {
+         System.out.println("Iniciando hilo principal.");
+         MiHilo mh=new MiHilo("#1");
+         mh.start();
+         
+         for (int i=0;i<50;i++){
+            System.out.print(".");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException exc) {
+                System.out.println("Hilo principal interrumpido");
+            }
+         }
+         
+         System.out.println("Hilo principal finalizado");
+     }
+ }
